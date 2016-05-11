fd85c2b63f3a2f97fc5632fd38d8f0094e8ef4bb
hendrikfrentrup <hendrik.frentrup@gmail.com>
2016-05-12 00:28:57 +0100

    Exercises 1-5. Recap of compiling, valgrind, etc.

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello World!");
+    puts("Hello Mars!");
+    puts("Hello Venus!");
+    puts("Hello Saturn!");
+    puts("Not you again, Mercury...");
+
+    return 0;
+}
---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int age=-2;
+	int height = 2;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d meters tall.\n", height);
+
+	return 0;
+}
